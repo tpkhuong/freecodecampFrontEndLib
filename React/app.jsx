4d587a60c7f85827b97b02b45c998b2e8e29680f
Toan <tpkhuong@gmail.com>
2021-01-23 07:33:59 -0500

    saving react codes for freecodecamp

--- React/app.jsx
+++ React/app.jsx
@@ -11,3 +11,278 @@ var JSX = (
     </ul>
   </div>
 );
+
+const JSX = (
+  <div>
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
+  </div>
+);
+
+ReactDOM.render(<JSX />, document.querySelector("#challenge-node"));
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello React!</h1>
+      </div>
+    );
+  }
+}
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* Change code below this line */}
+        <Fruits />
+        <Vegetables />
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
+
+// Change code below this line
+
+ReactDOM.render(<TypesOfFood />, document.querySelector("#challenge-node"));
+
+/* stateless functional component */
+
+var MyComponent = function returnElementToRender() {
+  return (
+    <div>
+      <h1>My First React Component!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(MyComponent, document.querySelector("#challenge-node"));
+
+/* stateless class component */
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My First React Component!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<MyComponent />, document.querySelector("#challenge-node"));
+
+var Camper = function defaultValue(props) {
+  return <p>{props.name}</p>;
+};
+
+Camper.defaultProp = {
+  name: "CamperBot",
+};
+
+Camper.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+class Camper extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <p>{this.props.name}</p>
+      </div>
+    );
+  }
+}
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "freeCodeCamp",
+    };
+  }
+  render() {
+    return (
+      <div>
+        {/* Change code below this line */}
+        <h1>{this.state.name}</h1>
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "freeCodeCamp",
+    };
+  }
+  render() {
+    // Change code below this line
+    var name = this.state.name;
+    // Change code above this line
+    return (
+      <div>
+        {/* Change code below this line */}
+        <h1>{name}</h1>
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Initial State",
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    // Change code below this line
+    this.setState({
+      name: "React Rocks!",
+    });
+    // Change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+}
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "Hello",
+    };
+    // Change code below this line
+    this.handleClick = this.handleClick.bind(this);
+    // Change code above this line
+  }
+  handleClick() {
+    this.setState({
+      text: "You clicked!",
+    });
+  }
+  render() {
+    return (
+      <div>
+        {/* Change code below this line */}
+        <button onClick={this.handleClick}>Click Me</button>
+        {/* Change code above this line */}
+        <h1>{this.state.text}</h1>
+      </div>
+    );
+  }
+}
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false,
+    };
+    // Change code below this line
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+    // Change code above this line
+  }
+  // Change code below this line
+  toggleVisibility() {
+    this.setState((state) => {
+      if (state.visibility === false) {
+        return { visibility: true };
+      } else {
+        return { visibility: false };
+      }
+    });
+  }
+  // Change code above this line
+  render() {
+    if (this.state.visibility) {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+          <h1>Now you see me!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+        </div>
+      );
+    }
+  }
+}
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    // Change code below this line
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+    // Change code above this line
+  }
+  // Change code below this line
+  increment() {
+    this.setState((state) => {
+      return (state.count += 1);
+    });
+  }
+  decrement() {
+    this.setState((state) => {
+      return (state.count -= 1);
+    });
+  }
+  reset() {
+    this.setState((state) => {
+      return (state.count = 0);
+    });
+  }
+  // Change code above this line
+  render() {
+    return (
+      <div>
+        <button className="inc" onClick={this.increment}>
+          Increment!
+        </button>
+        <button className="dec" onClick={this.decrement}>
+          Decrement!
+        </button>
+        <button className="reset" onClick={this.reset}>
+          Reset
+        </button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
+    );
+  }
+}
