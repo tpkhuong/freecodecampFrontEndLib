4613682365758a10573cfe958b86ea3d52bbe05a
Toan <tpkhuong@gmail.com>
2021-01-26 07:17:37 -0500

    saving code for React module of freecodecamp frontendLibraries

--- React/app.jsx
+++ React/app.jsx
@@ -648,3 +648,330 @@ class MagicEightBall extends React.Component {
     );
   }
 }
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true,
+    };
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState((state) => ({
+      display: !state.display,
+    }));
+  }
+  render() {
+    // Change code below this line
+
+    if (this.state.display === true) {
+      return (
+        <div>
+          <button onClick={this.toggleDisplay}>Toggle Display</button>
+          <h1>Displayed!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleDisplay}>Toggle Display</button>
+        </div>
+      );
+    }
+  }
+}
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true,
+    };
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState((state) => ({
+      display: !state.display,
+    }));
+  }
+  render() {
+    // Change code below this line
+
+    return (
+      <div>
+        {this.state.display && <h1>Displayed</h1>}
+        <button onClick={this.toggleDisplay}>Toggle Display</button>
+      </div>
+    );
+  }
+}
+
+const inputStyle = {
+  width: 235,
+  margin: 5,
+};
+
+class CheckUserAge extends React.Component {
+  constructor(props) {
+    super(props);
+    // Change code below this line
+    this.state = {
+      input: "",
+      userAge: "",
+    };
+    // Change code above this line
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+      userAge: "",
+    });
+  }
+  submit() {
+    this.setState((state) => ({
+      userAge: state.input,
+    }));
+  }
+  render() {
+    const buttonOne = <button onClick={this.submit}>Submit</button>;
+    const buttonTwo = <button>You May Enter</button>;
+    const buttonThree = <button>You Shall Not Pass</button>;
+    return (
+      <div>
+        <h3>Enter Your Age to Continue</h3>
+        <input
+          style={inputStyle}
+          type="number"
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+        <br />
+        {/* Change code below this line */}
+        {this.state.userAge === ""
+          ? buttonOne
+          : this.state.userAge < 18
+          ? buttonThree
+          : buttonTwo}
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
+
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    {
+      /* Change code below this line */
+    }
+    <h1>{this.props.fiftyFifty ? "You Win!" : "You Lose!"}</h1>;
+    {
+      /* Change code above this line */
+    }
+  }
+}
+
+class GameOfChance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({
+      counter: +1, // Change this line
+    });
+  }
+  render() {
+    const expression = Math.random() >= 0.5 ? true : false; // Change this line
+    return (
+      <div>
+        <button onClick={this.handleClick}>Play Again</button>
+        {/* Change code below this line */}
+        <Results fiftyFifty={expression} />
+        {/* Change code above this line */}
+        <p>{"Turn: " + this.state.counter}</p>
+      </div>
+    );
+  }
+}
+
+class GateKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value });
+  }
+  render() {
+    let inputStyle = {
+      border: "1px solid black",
+    };
+    // Change code below this line
+    if (this.state.input.length >= 15) {
+      inputStyle.border = "3px solid red";
+    }
+    // Change code above this line
+    return (
+      <div>
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+const textAreaStyles = {
+  width: 235,
+  margin: 5,
+};
+
+class MyToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    // Change code below this line
+    this.state = {
+      userInput: "",
+      toDoList: [],
+    };
+    // Change code above this line
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit() {
+    const itemsArray = this.state.userInput.split(",");
+    this.setState({
+      toDoList: itemsArray,
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value,
+    });
+  }
+  render() {
+    const items = this.state.toDoList.map(function makeLi(eachValue) {
+      return <li>{eachValue}</li>;
+    });
+    return (
+      <div>
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.userInput}
+          style={textAreaStyles}
+          placeholder="Separate Items With Commas"
+        />
+        <br />
+        <button onClick={this.handleSubmit}>Create List</button>
+        <h1>My "To Do" List:</h1>
+        <ul>{items}</ul>
+      </div>
+    );
+  }
+}
+
+const frontEndFrameworks = [
+  "React",
+  "Angular",
+  "Ember",
+  "Knockout",
+  "Backbone",
+  "Vue",
+];
+
+function randomIndex() {
+  return Math.random() * frontEndFrameworks.length;
+}
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map(
+    function makeElementWithUniqueIndex(eachValue, index) {
+      return <li key={randomIndex()}>{eachValue}</li>;
+    }
+  ); // Change this line
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>{renderFrameworks}</ul>
+    </div>
+  );
+}
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          username: "Jeff",
+          online: true,
+        },
+        {
+          username: "Alan",
+          online: false,
+        },
+        {
+          username: "Mary",
+          online: true,
+        },
+        {
+          username: "Jim",
+          online: false,
+        },
+        {
+          username: "Sara",
+          online: true,
+        },
+        {
+          username: "Laura",
+          online: true,
+        },
+      ],
+    };
+  }
+  render() {
+    const usersOnline = this.state.users.filter(function filterOnlineUsers(
+      eachValue
+    ) {
+      return eachValue.online === true;
+    }); // Change this line
+    const renderOnline = usersOnline.map(function makeElement(eachUser) {
+      return <li key={randomIndex()}>{eachUser.username}</li>;
+    }); // Change this line
+    return (
+      <div>
+        <h1>Current Online Users:</h1>
+        <ul>{renderOnline}</ul>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div />;
+  }
+}
+
+// Change code below this line
+
+ReactDOMServer.renderToString(<App />);
