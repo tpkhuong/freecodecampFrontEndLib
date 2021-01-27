4543510761560146db11f69a4ed72067ce731d9a
Toan <tpkhuong@gmail.com>
2021-01-27 09:10:03 -0500

    pushing redux.jsx code to git

--- React/redux.jsx
+++ React/redux.jsx
@@ -7,3 +7,227 @@ const reducer = (state = 5) => {
 // Define the store here:
 
 var store = Redux.createStore(reducer);
+
+const store = Redux.createStore((state = 5) => state);
+
+// Change code below this line
+
+var currentState = store.getState();
+
+var action = {
+  type: "LOGIN",
+};
+
+const action = {
+  type: "LOGIN",
+};
+// Define an action creator here:
+
+function actionCreator(objIput) {
+  return objInput;
+}
+
+actionCreator(action);
+
+const store = Redux.createStore((state = { login: false }) => state);
+
+const loginAction = () => {
+  return {
+    type: "LOGIN",
+  };
+};
+
+// Dispatch the action here:
+
+store.dispatch(loginAction());
+
+const defaultState = {
+  login: false,
+};
+
+const reducer = (state = defaultState, action) => {
+  // Change code below this line
+  if (action.type === "LOGIN") {
+    return { login: true };
+  }
+  return state;
+  // Change code above this line
+};
+
+const store = Redux.createStore(reducer);
+
+const loginAction = () => {
+  return {
+    type: "LOGIN",
+  };
+};
+
+const defaultState = {
+  authenticated: false,
+};
+
+const authReducer = (state = defaultState, action) => {
+  // Change code below this line
+  switch (action.type) {
+    case "LOGIN":
+      return { authenticated: true };
+
+    case "LOGOUT":
+      return { authenticated: false };
+
+    default:
+      return state;
+  }
+  // Change code above this line
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: "LOGIN",
+  };
+};
+
+const logoutUser = () => {
+  return {
+    type: "LOGOUT",
+  };
+};
+
+// Change code below this line
+const LOGIN = "LOGIN";
+const LOGOUT = "LOGOUT";
+// Change code above this line
+
+const defaultState = {
+  authenticated: false,
+};
+
+const authReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case LOGIN: // Change this line
+      return {
+        authenticated: true,
+      };
+    case LOGOUT: // Change this line
+      return {
+        authenticated: false,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: "LOGIN",
+  };
+};
+
+const logoutUser = () => {
+  return {
+    type: "LOGOUT",
+  };
+};
+
+const ADD = "ADD";
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+// Global count variable:
+let count = 0;
+
+// Change code below this line
+function increment() {
+  count += 1;
+}
+
+store.subscribe(increment);
+// Change code above this line
+
+store.dispatch({ type: ADD });
+console.log(count);
+store.dispatch({ type: ADD });
+console.log(count);
+store.dispatch({ type: ADD });
+console.log(count);
+
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const LOGIN = "LOGIN";
+const LOGOUT = "LOGOUT";
+
+const authReducer = (state = { authenticated: false }, action) => {
+  switch (action.type) {
+    case LOGIN:
+      return {
+        authenticated: true,
+      };
+    case LOGOUT:
+      return {
+        authenticated: false,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = Redux.combineReducers({
+  count: counterReducer,
+  auth: authReducer,
+}); // Define the root reducer here
+
+const store = Redux.createStore(rootReducer);
+
+const ADD_NOTE = "ADD_NOTE";
+
+const notesReducer = (state = "Initial State", action) => {
+  switch (action.type) {
+    // Change code below this line
+    case ADD_NOTE:
+      return action.text;
+    // Change code above this line
+    default:
+      return state;
+  }
+};
+
+const addNoteText = (note) => {
+  // Change code below this line
+  return {
+    type: ADD_NOTE,
+    text: note,
+  };
+  // Change code above this line
+};
+
+const store = Redux.createStore(notesReducer);
+
+console.log(store.getState());
+store.dispatch(addNoteText("Hello!"));
+console.log(store.getState());
