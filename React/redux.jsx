0eafa08a5a0a34fc75b5654bdac90d02c1275c26
Toan <tpkhuong@gmail.com>
2021-02-02 08:03:36 -0500

    saving point for redux.jsx

--- React/redux.jsx
+++ React/redux.jsx
@@ -412,3 +412,31 @@ const wakeUp = () => {
 };
 
 const store = Redux.createStore(immutableReducer);
+
+const INCREMENT = "INCREMENT"; // Define a constant for increment action types
+const DECREMENT = "DECREMENT"; // Define a constant for decrement action types
+
+const counterReducer = function (state = 0, action) {
+  switch (action.type) {
+    case INCREMENT:
+      return (state += 1);
+    case DECREMENT:
+      return (state -= 1);
+    default:
+      return state;
+  }
+}; // Define the counter reducer which will increment or decrement the state based on the action it receives
+
+const incAction = function increByOne() {
+  return {
+    type: INCREMENT,
+  };
+}; // Define an action creator for incrementing
+
+const decAction = function decreByOne() {
+  return {
+    type: DECREMENT,
+  };
+}; // Define an action creator for decrementing
+
+const store = Redux.createStore(counterReducer); // Define the Redux store here, passing in your reducers
