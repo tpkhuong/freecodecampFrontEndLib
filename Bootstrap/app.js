f240b817cf2c939167e7329b62ffcf350c810674
Toan <tpkhuong@gmail.com>
2021-02-03 07:21:10 -0500

    finished challenge 10, start on challenege 11

--- Bootstrap/app.js
+++ Bootstrap/app.js
@@ -309,6 +309,8 @@ function multiMap(arrOfVals, arrOfCallbacks) {
     },
     {}
   );
+
+  return objOfArrAsValues;
 }
 
 var ourStrings = ["catfood", "marvel", "drinks"];
@@ -319,9 +321,24 @@ function toUpperCase(strInput) {
 }
 
 function capitalizeFirstStr(strInput) {
-  return strInput.charAt(0).toUpperCase() + strInput.slice(1).tolowerCase();
+  return strInput.charAt(0).toUpperCase() + strInput.slice(1).toLowerCase();
 }
 
 function concatSameWord(strInput) {
   return `${strInput}${strInput}`;
 }
+
+/***
+ * Challenge 11
+Construct a function objectFilter that accepts an object as the first parameter and a callback function as the second parameter. objectFilter will return a new object.
+The new object will contain only the properties from the input object such that the property's value is equal to the property's key passed into the callback.
+ *  ***/
+
+// const cities = {
+// London: 'LONDON',
+// LA: 'Los Angeles',
+// Paris: 'PARIS',
+// };
+// console.log(objectFilter(cities, city => city.toUpperCase())) // Should log { London: 'LONDON', Paris: 'PARIS'}
+
+function objectFilter(objInput, callback) {}
