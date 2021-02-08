60afda4d2d49443163f5d24a3c848e5a524b1301
Toan <tpkhuong@gmail.com>
2021-02-08 07:13:08 -0500

    saving where we left off on freecode react module

--- React/app.jsx
+++ React/app.jsx
@@ -1120,3 +1120,38 @@ class ToDo extends React.Component {
     );
   }
 }
+
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper />
+      </div>
+    );
+  }
+};
+// Change code below this line
+
+
+
+class Camper extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <p>{ this.props.name}</p>
+    )
+  }
+}
+
+Camper.defaultProps = {
+  name: "CamperBot"
+}
+
+Camper.propTypes = {
+  name: PropTypes.string.isRequired
+}
