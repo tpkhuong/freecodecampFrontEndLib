b3b58124c0e74f9ed556984964f9d10fe27d69bf
Toan <tpkhuong@gmail.com>
2021-02-12 08:12:48 -0500

    challenge 6 completed for cs bin closure module

--- app.js
+++ app.js
@@ -60,3 +60,83 @@ function addByX(numInput) {
 
   return incrementByNumInput;
 }
+
+/*
+Challenge 4
+Write a function once that accepts a callback as input and returns a function. When the returned function is called the first time, it should call the callback and return that output.
+If it is called any additional times, instead of calling the callback again it will simply return the output value from the first time it was called.
+*/
+
+// /*** Uncomment these to check your work! ***/
+// const onceFunc = once(addByTwo);
+// console.log(onceFunc(4));  // => should log 6
+// console.log(onceFunc(10));  // => should log 6
+// console.log(onceFunc(9001));  // => should log 6
+
+function once(callbackFunc) {
+  var counter = 0;
+  let result;
+
+  return function innerFunc(valueInput) {
+    if (counter == 0) {
+      result = callbackFunc(valueInput);
+      counter += 1;
+      return result;
+    } else {
+      return result;
+    }
+  };
+}
+
+function addByTwo(numInput) {
+  return (numInput += 2);
+}
+
+/*
+Challenge 5
+Write a function after that takes the number of times the callback needs to be called before being executed as the first parameter and the callback as the second parameter.
+*/
+
+// /*** Uncomment these to check your work! ***/
+// const called = function() { console.log('hello') };
+// const afterCalled = after(3, called);
+// afterCalled(); // => nothing is printed
+// afterCalled(); // => nothing is printed
+// afterCalled(); // => 'hello' is printed
+
+function after(numOfTimeCalls, callbacFunc) {
+  var counter = 0;
+
+  return function innerFunc() {
+    counter += 1;
+    if (counter == numOfTimeCalls) {
+      let result = callbacFunc();
+      return result;
+    }
+  };
+}
+
+/*
+Challenge 6
+Write a function delay that accepts a callback as the first parameter and the wait in milliseconds before allowing the callback to be invoked as the second parameter.
+Any additional arguments after wait are provided to func when it is invoked. HINT: research setTimeout();
+*/
+
+function delay(callbackFunc, waitInMillisec, ...listOfArgs) {
+  setTimeout(function () {
+    callbackFunc(...listOfArgs);
+  }, waitInMillisec);
+}
+
+delay(
+  (...input) => {
+    input.forEach(function print(eachValue) {
+      console.log(eachValue);
+    });
+  },
+  500,
+  "hello",
+  "world",
+  "cat",
+  "dog"
+);
