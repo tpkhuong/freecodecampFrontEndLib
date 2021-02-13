ac88ea1b6e8f3cb56bbc4311a1feff90ff070170
Toan <tpkhuong@gmail.com>
2021-02-13 07:37:36 -0500

    completed challenge 9 of cs bin

--- app.js
+++ app.js
@@ -140,3 +140,104 @@ delay(
   "cat",
   "dog"
 );
+
+/*
+Challenge 7
+
+Write a function rollCall that accepts an array of names and returns a function. The first time the returned function is invoked, it should log the first name to the console.
+The second time it is invoked, it should log the second name to the console, and so on, until all names have been called. Once all names have been called, it should log 'Everyone accounted for'.
+
+*/
+
+// /*** Uncomment these to check your work! ***/
+// const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth'])
+// rollCaller() // => should log 'Victoria'
+// rollCaller() // => should log 'Juan'
+// rollCaller() // => should log 'Ruth'
+// rollCaller() // => should log 'Everyone accounted for'
+
+function rollCall(arrInput) {
+  var index = 0;
+  var lengthOfArr = arrInput.length;
+
+  function innerFunc() {
+    var printMe = arrInput[index];
+    /* when we have the index + = 1 ahead of our ternary when printMe = arrInput[2] it will assign "Ruth" to printMe BUT
+    our code index += 1 will add 1 to the index which makes it 3 that will satisfied our condition of index == lengthOfArr so it will assign "everyone account for"
+    to logThis
+    */
+    index += 1;
+    var logThis = index == lengthOfArr ? "Everyone account for" : printMe;
+    console.log(logThis);
+  }
+
+  return innerFunc;
+}
+
+/*
+
+Challenge 8
+
+Create a function saveOutput that accepts a function (that will accept one argument), and a string (that will act as a password).
+saveOutput will then return a function that behaves exactly like the passed-in function, except for when the password string is passed in as an argument.
+When this happens, the returned function will return an object with all previously passed-in arguments as keys, and the corresponding outputs as values.
+
+*/
+
+// /*** Uncomment these to check your work! ***/
+// const multiplyBy2 = function(num) { return num * 2; };
+// const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+// console.log(multBy2AndLog(2)); // => should log 4
+// console.log(multBy2AndLog(9)); // => should log 18
+// console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
+
+function saveOutput(callbackFunc, passwordStr) {
+  var returnThisObj = {};
+
+  function innerFunc(inputForCallback) {
+    if (inputForCallback == passwordStr) {
+      console.log(returnThisObj);
+    } else {
+      var resultFromInvokingCallback = callbackFunc(inputForCallback);
+
+      returnThisObj = Object.assign(returnThisObj, {
+        [inputForCallback]: resultFromInvokingCallback,
+      });
+      console.log(resultFromInvokingCallback);
+    }
+  }
+
+  return innerFunc;
+}
+
+/* 
+Challenge 9
+Create a function cycleIterator that accepts an array, and returns a function. The returned function will accept zero arguments.
+When first invoked, the returned function will return the first element of the array. When invoked a second time, the returned function will return the second element of the array, and so forth.
+After returning the last element of the array, the next invocation will return the first element of the array again, and continue on with the second after that, and so forth.
+
+*/
+
+// /*** Uncomment these to check your work! ***/
+// const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
+// const getDay = cycleIterator(threeDayWeekend);
+// console.log(getDay()); // => should log 'Fri'
+// console.log(getDay()); // => should log 'Sat'
+// console.log(getDay()); // => should log 'Sun'
+// console.log(getDay()); // => should log 'Fri'
+
+function cycleIterator(arrInput) {
+  var index = 0;
+  var lengthOfArr = arrInput.length;
+
+  function innerFunc() {
+    if (index == lengthOfArr) {
+      index = 0;
+    }
+    var itemInArr = arrInput[index];
+    index += 1;
+    return itemInArr;
+  }
+
+  return innerFunc;
+}
