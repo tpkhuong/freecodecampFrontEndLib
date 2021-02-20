83a80d2ab7e587c87cc224f44d18cdc4f7c9af61
Toan <tpkhuong@gmail.com>
2021-02-20 06:30:17 -0500

    working on challenge 17 cs bin

--- app.js
+++ app.js
@@ -567,3 +567,29 @@ function average() {
   }
   return innerFunc;
 }
+
+/*
+
+Challenge 17
+Create a function makeFuncTester that accepts an array (of two-element sub-arrays), and returns a function (that will accept a callback).
+The returned function should return true if the first elements (of each sub-array) being passed into the callback all yield the corresponding second elements (of the same sub-array).
+Otherwise, the returned function should return false.
+
+*/
+
+// /*** Uncomment these to check your work! ***/
+// const capLastTestCases = [];
+// capLastTestCases.push(['hello', 'hellO']);
+// capLastTestCases.push(['goodbye', 'goodbyE']);
+// capLastTestCases.push(['howdy', 'howdY']);
+// const shouldCapitalizeLast = makeFuncTester(capLastTestCases);
+// const capLastAttempt1 = str => str.toUpperCase();
+// const capLastAttempt2 = str => str.slice(0, -1) + str.slice(-1).toUpperCase();
+// console.log(shouldCapitalizeLast(capLastAttempt1)); // => should log false
+// console.log(shouldCapitalizeLast(capLastAttempt2)); // => should log true
+
+function makeFuncTest(arrInput) {
+  function innerFunc(callackFunc) {}
+
+  return innerFunc;
+}
