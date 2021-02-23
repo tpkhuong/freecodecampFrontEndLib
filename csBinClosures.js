58df473ddb04e01ff963dba57b7cd1e51e7626f3
Toan <tpkhuong@gmail.com>
2021-02-23 07:20:55 -0500

    tracking renamed file app.js to csBinClosure

--- csBinClosures.js
+++ csBinClosures.js
@@ -654,3 +654,62 @@ function makeHistory(limitInput) {
 
   return innerFunc;
 }
+
+/*
+
+Create a function blackjack that accepts an array (which will contain numbers ranging from 1 through 11), and returns a DEALER function.
+The DEALER function will take two arguments (both numbers), and then return yet ANOTHER function, which we will call the PLAYER function.
+On the FIRST invocation of the PLAYER function, it will return the sum of the two numbers passed into the DEALER function.
+
+On the SECOND invocation of the PLAYER function, it will return either:
+
+the first number in the array that was passed into blackjack PLUS the sum of the two numbers passed in as arguments into the DEALER function, IF that sum is 21 or below, OR
+the string 'bust' if that sum is over 21.
+
+If it is 'bust', then every invocation of the PLAYER function AFTER THAT will return the string 'you are done!' (but unlike 'bust', the 'you are done!'
+output will NOT use a number in the array). If it is NOT 'bust', then the next invocation of the PLAYER function will return either:
+
+the most recent sum plus the next number in the array (a new sum) if that new sum is 21 or less, OR
+the string 'bust' if the new sum is over 21.
+
+And again, if it is 'bust', then every subsequent invocation of the PLAYER function will return the string 'you are done!'. Otherwise, it can continue on to give the next sum with the next number in the array, and so forth.
+You may assume that the given array is long enough to give a 'bust' before running out of numbers.
+
+BONUS: Implement blackjack so the DEALER function can return more PLAYER functions that will each continue to take the next number in the array after the previous PLAYER function left off.
+You will just need to make sure the array has enough numbers for all the PLAYER functions.
+
+*/
+
+// /*** Uncomment these to check your work! ***/
+
+// /*** DEALER ***/
+// const deal = blackjack([2, 6, 1, 7, 11, 4, 6, 3, 9, 8, 9, 3, 10, 4, 5, 3, 7, 4, 9, 6, 10, 11]);
+
+// /*** PLAYER 1 ***/
+// const i_like_to_live_dangerously = deal(4, 5);
+// console.log(i_like_to_live_dangerously()); // => should log 9
+// console.log(i_like_to_live_dangerously()); // => should log 11
+// console.log(i_like_to_live_dangerously()); // => should log 17
+// console.log(i_like_to_live_dangerously()); // => should log 18
+// console.log(i_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
+// console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
+
+// /*** BELOW LINES ARE FOR THE BONUS ***/
+
+// /*** PLAYER 2 ***/
+// const i_TOO_like_to_live_dangerously = deal(2, 2);
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 4
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 15
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 19
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
+
+// /*** PLAYER 3 ***/
+// const i_ALSO_like_to_live_dangerously = deal(3, 7);
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 10
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 13
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
