59095e9e547d0d97b77ddb0e044234ae730bfc7d
Toan <tpkhuong@gmail.com>
2021-03-05 06:20:52 -0500

    redux module redone

--- React/redux.jsx
+++ React/redux.jsx
@@ -561,3 +561,179 @@ var currentState = store.getState();
 var action = {
   type: "LOGIN",
 };
+
+function actionCreator() {
+  return action;
+}
+
+const defaultState = {
+  login: false,
+};
+
+const reducer = (state = defaultState, action) => {
+  // Change code below this line
+  if (action.type == "LOGIN") {
+    return {
+      login: true,
+    };
+  }
+  return state;
+  // Change code above this line
+};
+
+const store = Redux.createStore(reducer);
+
+const loginAction = () => {
+  return {
+    type: "LOGIN",
+  };
+};
+
+const defaultState = {
+  authenticated: false,
+};
+
+const authReducer = (state = defaultState, action) => {
+  // Change code below this line
+  switch (action.type) {
+    case "LOGIN":
+      return { authenticated: true };
+    case "LOGOUT":
+      return { authenticated: false };
+    default:
+      return state;
+  }
+  // Change code above this line
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: "LOGIN",
+  };
+};
+
+const logoutUser = () => {
+  return {
+    type: "LOGOUT",
+  };
+};
+
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const LOGIN = "LOGIN";
+const LOGOUT = "LOGOUT";
+
+const authReducer = (state = { authenticated: false }, action) => {
+  switch (action.type) {
+    case LOGIN:
+      return {
+        authenticated: true,
+      };
+    case LOGOUT:
+      return {
+        authenticated: false,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = Redux.combineReducers({
+  count: counterReducer,
+  auth: authReducer,
+}); // Define the root reducer here
+
+const store = Redux.createStore(rootReducer);
+
+const ADD_NOTE = "ADD_NOTE";
+
+const notesReducer = (state = "Initial State", action) => {
+  switch (action.type) {
+    // Change code below this line
+    case ADD_NOTE:
+      return action.text;
+    // Change code above this line
+    default:
+      return state;
+  }
+};
+
+const addNoteText = (note) => {
+  // Change code below this line
+  return {
+    type: ADD_NOTE,
+    text: note,
+  };
+  // Change code above this line
+};
+
+const store = Redux.createStore(notesReducer);
+
+console.log(store.getState());
+store.dispatch(addNoteText("Hello!"));
+console.log(store.getState());
+
+const REQUESTING_DATA = "REQUESTING_DATA";
+const RECEIVED_DATA = "RECEIVED_DATA";
+
+const requestingData = () => {
+  return { type: REQUESTING_DATA };
+};
+const receivedData = (data) => {
+  return { type: RECEIVED_DATA, users: data.users };
+};
+
+const handleAsync = () => {
+  return function (dispatch) {
+    // Dispatch request action here
+    dispatch(requestingData());
+    setTimeout(function () {
+      let data = {
+        users: ["Jeff", "William", "Alice"],
+      };
+      dispatch(receivedData());
+      // Dispatch received data action here
+    }, 2500);
+  };
+};
+
+const defaultState = {
+  fetching: false,
+  users: [],
+};
+
+const asyncDataReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case REQUESTING_DATA:
+      return {
+        fetching: true,
+        users: [],
+      };
+    case RECEIVED_DATA:
+      return {
+        fetching: false,
+        users: action.users,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(
+  asyncDataReducer,
+  Redux.applyMiddleware(ReduxThunk.default)
+);
