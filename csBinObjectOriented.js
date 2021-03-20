bdc917f3d9b41eceb9cd8d36e95399b5768d001e
Toan <tpkhuong@gmail.com>
2021-03-20 06:42:13 -0400

    tracking csbinobject file

---
+++ csBinObjectOriented.js
@@ -0,0 +1,13 @@
+/*
+
+Working with Object Literals
+
+Challenge 1
+
+Create a function that has two parameters (name and age) and returns an object. Let's call this function makePerson. This function will:
+Create an empty object
+Add a name property to the newly created object with its value being the 'name' argument passed into the function
+Add an age property to the newly created object with its value being the 'age' argument passed into the function
+Return the object
+
+*/
