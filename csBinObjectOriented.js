6e2968fe0add1ee3db3890fb29d4d0583c2406ac
Toan <tpkhuong@gmail.com>
2021-03-22 06:41:50 -0400

    challenge 9 done csbin oop

--- csBinObjectOriented.js
+++ csBinObjectOriented.js
@@ -140,3 +140,68 @@ function personFromConstructor(name, age) {
     return `Hello`;
   };
 }
+
+/*
+
+Challenge 7
+Without editing the code you've already written, add an introduce method to the PersonConstructor function that logs "Hi, my name is [name]".
+
+// mike.introduce(); // -> Logs 'Hi, my name is Mike'
+*/
+
+personFromConstructor.prototype.introduce = function () {
+  return `Hi, my name is ${this.name}`;
+};
+
+/****************************************************************
+                        USING ES6 CLASSES
+****************************************************************/
+
+/*
+
+Challenge 8
+Create a class PersonClass. PersonClass should have a constructor that is passed an input of name and saves it to a property by the same name. 
+ersonClass should also have a method called greet that logs the string 'hello'.
+
+*/
+
+// /********* Uncomment this line to test your work! *********/
+// george.greet(); // -> Logs 'hello'
+
+class PersonClass {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  greet() {
+    return `Hello`;
+  }
+}
+
+const george = new PersonClass();
+
+/*
+
+Challenge 9
+Create a class DeveloperClass that creates objects by extending the PersonClass class. In addition to having a name property and greet method,
+DeveloperClass should have an introduce method. When called, introduce should log the string 'Hello World, my name is [name]'.
+
+*/
+
+/*** CHALLENGE 9 ***/
+
+// add code here
+
+// /********* Uncomment these lines to test your work! *********/
+// const thai = new DeveloperClass('Thai', 32);
+// console.log(thai.name); // -> Logs 'Thai'
+// thai.introduce(); //-> Logs 'Hello World, my name is Thai'
+
+class DeveloperClass extends PersonClass {
+  constructor(name, age) {
+    super(name, age);
+  }
+  introduce() {
+    return `Hello World, my name is ${this.name}. I am ${this.age} year old`;
+  }
+}
