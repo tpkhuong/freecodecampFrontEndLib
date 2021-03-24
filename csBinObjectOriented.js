1762968bc3c91ced4bd63ee2ce8eaf2424493e29
Toan <tpkhuong@gmail.com>
2021-03-24 06:32:44 -0400

    completed challenge 14 csbin oop

--- csBinObjectOriented.js
+++ csBinObjectOriented.js
@@ -275,4 +275,34 @@ function adminFactory(name, score) {
 // Created a method called sharePublicMessage that logs 'Welcome users!' and will be available to adminFactory objects,
 // but not userFactory objects.Do not add this method directly in the adminFactory function.
 
+adminFunctionStore.sharePublicMessage = function () {
+  return `${this.name} says Welcome all!`;
+};
+
 /***** subclassing *****/
+
+/***** extension: Mixins *****/
+
+// Mixins are a tool in object-oriented programming that allows objects to be given methods and properties beyond those provided by their inheritance.
+// For this challenge, complete the missing code, giving all of the robotMixin properties to robotFido.
+// Do this in a single line of code, without adding the properties individually.
+
+class Dog {
+  constructor() {
+    this.legs = 4;
+  }
+  speak() {
+    console.log("Woof!");
+  }
+}
+
+const robotMixin = {
+  skin: "metal",
+  speak: function () {
+    console.log(`I have ${this.legs} legs and am made of ${this.skin}`);
+  },
+};
+
+let robotFido = new Dog();
+
+/***** extension: Mixins *****/
