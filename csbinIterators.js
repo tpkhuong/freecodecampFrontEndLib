c68ca496b875fd76057355564c8c58a161f7134b
Toan <tpkhuong@gmail.com>
2021-03-29 07:05:42 -0400

    tracking csbinIterators

---
+++ csbinIterators.js
@@ -0,0 +1,11 @@
+/*
+
+Challenge 1
+A) Create a for loop that iterates through an array and returns the sum of the elements of the array.
+B) Create a functional iterator for an array that returns each value of the array when called, one element at a time.
+
+
+// Uncomment the lines below to test your work
+// const array = [1, 2, 3, 4];
+// console.log(sumFunc(array)); // -> should log 10
+*/
