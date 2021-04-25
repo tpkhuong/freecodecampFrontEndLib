900771010be1a5b30cc12dfcb513324d39eb1fe2
Toan <tpkhuong@gmail.com>
2021-04-25 06:55:22 -0400

    making progress implementing debounce

--- csBinAsyncTwo.js
+++ csBinAsyncTwo.js
@@ -270,15 +270,29 @@ Additional calls to the returned function within the interval time should not be
 // setTimeout(function() { console.log(giveHiSometimes()); }, 4000); // -> undefined
 // setTimeout(function() { console.log(giveHiSometimes()); }, 8000); // -> 'hi'
 
+Creates a debounced function that delays invoking func until after wait milliseconds have elapsed since the last time the debounced function was invoked.
+The debounced function comes with a cancel method to cancel delayed func invocations and a flush method to immediately invoke them. Provide options to indicate whether
+func should be invoked on the leading and/or trailing edge of the wait timeout. The func is invoked with the last arguments provided to the debounced function.
+Subsequent calls to the debounced function return the result of the last func invocation.
+
+Note: If leading and trailing options are true, func is invoked on the trailing edge of the timeout only if the debounced function is invoked more than once during the wait timeout.
+
+If wait is 0 and leading is false, func invocation is deferred until to the next tick, similar to setTimeout with a timeout of 0.
+
 */
 
-function debounce(callback, delay) {
+function debounce(callback) {
   var counter = 0;
   function innerFunc(delayOfCalls) {
-    var [firstValue] = String(delay);
-    var convertToNum = Number(firstValue);
+    // var [firstValue] = String(delay);
+    // var convertToNum = Number(firstValue);
 
-    callback();
+    var callThisToCancel = setTimeout(() => {
+      // console.log(delayOfCalls);
+      // console.log(callback);
+      callback();
+    }, 3000);
+    //we can cancel the setTimeout if the time between the evocation of the func is not between the dalay/wait time passed in.
     // run the return function when counter is greater than delay
   }
 
@@ -286,6 +300,7 @@ function debounce(callback, delay) {
 }
 
 function giveHi() {
+  console.log("hello world");
   return "Hi";
 }
 
